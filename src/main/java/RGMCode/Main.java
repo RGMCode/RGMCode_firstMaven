ce5a7ad98bc47f53a3d6fce8b0f1de23d929b835
mrg <rgmengedoth@gmail.com>
2023-07-06 11:00:20 +0200

    add

--- src/main/java/RGMCode/Main.java
+++ src/main/java/RGMCode/Main.java
@@ -15,4 +15,26 @@ public class Main {
         return checkNum > 100;
     }
 
+
+
+    /*User gibt sein Alter ein und nach einer rüfung werden
+    * folgende Werte zurückgegeben
+    * unter 14 = Kind
+    * zwischen 14 und 16 = Minderjährig
+    * zwischen 16 und 18 = Jugendlich
+    * 18+ Erwachsen
+    * */
+
+    public static String checkAge(int age) {
+        if (age < 16 && age > 13){
+            return "Minderjährig";
+        } else if (age < 18 && age > 15) {
+            return "Jugendlich";
+        } else if (age >= 18) {
+            return "Erwachsen";
+        }
+        return "Kind";
+    }
+
+
 }
--- src/test/java/RGMCode/MainTest.java
+++ src/test/java/RGMCode/MainTest.java
@@ -31,4 +31,62 @@ class MainTest {
         assertTrue(act);
     }
 
+
+    /*User gibt sein Alter ein und nach einer rüfung werden folgende Werte zurückgegeben
+     * unter 14 = Kind
+     * zwischen 14 und 16 = Minderjährig
+     * zwischen 16 und 18 = Jugendlich
+     * 18+ Erwachsen
+     * */
+
+    @Test
+    void whenCheckAge_with10_thenExpectKind(){
+        //GIVEN
+        int age = 10;
+
+        //WHEN
+        String actual = Main.checkAge(age);
+
+        //THEN
+        assertEquals("Kind", actual);
+    }
+
+    @Test
+    void whenCheckAge_with14_thenExpectMinderjaehrig(){
+        //GIVEN
+        int age = 14;
+
+        //WHEN
+        String actual = Main.checkAge(age);
+
+        //THEN
+        assertEquals("Minderjährig", actual);
+    }
+
+    @Test
+    void whenCheckAge_with16_thenExpectJugendlich(){
+        //GIVEN
+        int age = 16;
+
+        //WHEN
+        String actual = Main.checkAge(age);
+
+        //THEN
+        assertEquals("Jugendlich", actual);
+    }
+
+    @Test
+    void whenCheckAge_with18_thenExpectErwachsen(){
+        //GIVEN
+        int age = 18;
+
+        //WHEN
+        String actual = Main.checkAge(age);
+
+        //THEN
+        assertEquals("Erwachsen", actual);
+    }
+
+
+
 }
